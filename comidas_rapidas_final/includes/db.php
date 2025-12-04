eef0b48b9cd69a2a68f6122124f28edf176aae2d
zof202007 <zof202007@gmail.com>
2025-12-03 22:15:33 -0500

    Update db.php
    correcion

--- comidas_rapidas_final/includes/db.php
+++ comidas_rapidas_final/includes/db.php
@@ -1,13 +1,20 @@
 <?php
 $host = "localhost";
-$db = "comidas_rapidas";
+$db   = "comidas_rapidas";
 $user = "root";
-$pass = "";
+$pass = ""; // En producción debe usarse contraseña segura
+
 
 try {
-    $pdo = new PDO("mysql:host=$host;dbname=$db;charset=utf8", $user, $pass);
+    $pdo = new PDO(
+        "mysql:host=$host;dbname=$db;charset=utf8",
+        $user,
+        $pass,
+        [
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+        ]
+    );
 } catch (PDOException $e) {
     die("Error de conexión: " . $e->getMessage());
 }
-?>
-
