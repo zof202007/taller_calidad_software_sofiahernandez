75e2d2b81fbc1f05b6b13c0e5c67e93742c974db
zof202007 <zof202007@gmail.com>
2025-12-03 22:03:25 -0500

    Update editar.php
    correcion editartip

--- comidas_rapidas_final/crud_tipos/editar.php
+++ comidas_rapidas_final/crud_tipos/editar.php
@@ -1,7 +1,5 @@
-<link rel="stylesheet" href="/comidas_rapidas_final/css/tipos.css">
-<div class="container">
 <?php
-require "../includes/db.php";
+require_once "../includes/db.php";
 
 $id = $_GET["id"];
 
@@ -16,16 +14,24 @@ if ($_POST) {
     $sql->execute([$nombre, $id]);
 
     header("Location: index.php");
+    exit();
 }
 ?>
+
+<link rel="stylesheet" href="/comidas_rapidas_final/css/tipos.css">
+<div class="container">
 
 <h2>Editar Tipo</h2>
 
 <form method="POST">
-    <input type="text" name="nombre" value="<?php echo $tipo["nombre_tipo"]; ?>" required>
+    <input type="text" name="nombre" value="<?= $tipo["nombre_tipo"]; ?>" required>
     <br><br>
     <button type="submit">Actualizar</button>
 </form>
 
 <br>
+<a href="index.php">⬅ Volver</a>
+
+</div>
+
 <a href="index.php">⬅ Volver</a>
